ef70fcae1ae0d93fc28811ec43ff0831d3a4aaa2
CristhianMorenoG <morenocristhian@outlook.com>
2021-10-24 15:33:55 -0500

    Finalizacion proyecto

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/api/usuario', router);
+app.use('/api', router);
 
 app.set('port', process.env.PORT || 3000);
 
